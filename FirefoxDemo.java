a313205bf9e0d20252a0b7ffc8c8721d959bc222
weqas1994 <weqas1994@gmail.com>
2019-06-13 21:16:24 -0400

    weqas

---
+++ FirefoxDemo.java
@@ -0,0 +1,18 @@
+package com.syntax.class1;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class FirefoxDemo {
+
+	public static void main(String[] args) {
+
+
+		System.setProperty("webdriver.gecko.driver", "C:\\Selenium\\Drivers\\geckodriver.exe");
+		WebDriver driver=new FirefoxDriver();
+		driver.get("https://google.com");
+		System.out.println(driver.getTitle());
+		driver.close();
+	}
+
+}
